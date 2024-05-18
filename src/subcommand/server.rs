5ecd9909acbe9eb6bae13f48bb2a93ae763f2b81
bouzuya <m@bouzuya.net>
2024-05-18 22:12:36 +0900

    Add GET / handler

--- src/subcommand/server.rs
+++ src/subcommand/server.rs
@@ -1,4 +1,7 @@
 pub async fn run() -> anyhow::Result<()> {
-    println!("FIXME: run server");
+    let router = axum::Router::new().route("/", axum::routing::get(|| async { "OK" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, router).await?;
     Ok(())
 }
