2b41c27c5e89c900dc862c866d6ca122b4f8e8e3
bouzuya <m@bouzuya.net>
2024-05-19 07:37:51 +0900

    Rename show_root command

--- src/subcommand/server/handler.rs
+++ src/subcommand/server/handler.rs
@@ -1,7 +1,7 @@
 mod create_chart;
 mod delete_chart;
 mod get_chart;
-mod show_root;
+mod get_root;
 mod update_chart;
 
 use axum::Router;
@@ -20,6 +20,6 @@ pub fn router<
         .merge(create_chart::router())
         .merge(delete_chart::router())
         .merge(get_chart::router())
-        .merge(show_root::router())
+        .merge(get_root::router())
         .merge(update_chart::router())
 }
