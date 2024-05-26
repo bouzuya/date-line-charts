da4985952f0985a93ab72864830b08d2129e8c42
bouzuya <m@bouzuya.net>
2024-05-26 20:47:34 +0900

    Add query_use_case::port::data_point_reader

--- crates/query_use_case/src/query_use_case/port.rs
+++ crates/query_use_case/src/query_use_case/port.rs
@@ -1,3 +1,5 @@
 mod chart_reader;
+mod data_point_reader;
 
 pub use self::chart_reader::{ChartQueryData, ChartReader, HasChartReader};
+pub use self::data_point_reader::{DataPointQueryData, DataPointReader, HasDataPointReader};
---
+++ crates/query_use_case/src/query_use_case/port/data_point_reader.rs
@@ -0,0 +1,27 @@
+use std::sync::Arc;
+
+use write_model::value_object::{ChartId, DateTime, XValue, YValue};
+
+#[derive(Clone)]
+pub struct DataPointQueryData {
+    pub chart_id: ChartId,
+    pub created_at: DateTime,
+    pub x_value: XValue,
+    pub y_value: YValue,
+}
+
+#[async_trait::async_trait]
+pub trait DataPointReader {
+    async fn get(
+        &self,
+        id: DataPointId,
+    ) -> Result<DataPointQueryData, Box<dyn std::error::Error + Send + Sync>>;
+    async fn list(
+        &self,
+        chart_id: ChartId,
+    ) -> Result<Vec<DataPointQueryData>, Box<dyn std::error::Error + Send + Sync>>;
+}
+
+pub trait HasDataPointReader {
+    fn data_point_reader(&self) -> Arc<dyn DataPointReader + Send + Sync>;
+}
