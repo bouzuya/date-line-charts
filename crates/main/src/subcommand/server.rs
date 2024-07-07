048e4fb37c029e2f173367b1560363fc33b12e40
bouzuya <m@bouzuya.net>
2024-07-08 06:57:42 +0900

    Fix to use FirestoreChartStore

--- crates/main/src/subcommand/server.rs
+++ crates/main/src/subcommand/server.rs
@@ -1,9 +1,14 @@
 use std::{path::PathBuf, sync::Arc};
 
-use file_system_store::{FileSystemChartStore, FileSystemDataPointStore};
+use file_system_store::FileSystemDataPointStore;
+use firestore_store::FirestoreChartStore;
 
 pub async fn run() -> anyhow::Result<()> {
-    let chart_store = Arc::new(FileSystemChartStore::new(PathBuf::from("data")));
+    let chart_store = Arc::new(
+        FirestoreChartStore::new()
+            .await
+            .map_err(|e| anyhow::anyhow!(e))?,
+    );
     let data_point_store = Arc::new(FileSystemDataPointStore::new(PathBuf::from("data")));
     let app = server::App::new(
         chart_store.clone(),
