529dda8ec090d845170ec45e2e71d374986ed629
bouzuya <m@bouzuya.net>
2024-07-12 20:47:40 +0900

    Fix FirestoreChartStore to use FirestoreEventStore

--- crates/firestore_store/src/firestore_store/firestore_chart_store.rs
+++ crates/firestore_store/src/firestore_store/firestore_chart_store.rs
@@ -1,25 +1,32 @@
 use std::{future::Future, pin::Pin, str::FromStr};
 
 use crate::{
-    converter, path,
+    converter,
+    firestore_event_store::FirestoreEventStore,
+    path,
     schema::{
         self, ChartDocumentData, ChartEventDataDocumentData, EventDocumentData,
-        EventStreamDocumentData, UpdaterMetadataDocumentData,
-        UpdaterMetadataProcessedEventDocumentData,
+        UpdaterMetadataDocumentData, UpdaterMetadataProcessedEventDocumentData,
     },
 };
 use firestore_client::{FieldPath, Filter, FirestoreClient, Precondition, Transaction};
 use write_model::{
     aggregate::Chart,
-    event::ChartEvent,
+    event::{ChartEvent, Event},
     value_object::{ChartId, EventId, EventStreamId, Version},
 };
 
-pub struct FirestoreChartStore(FirestoreClient);
+pub struct FirestoreChartStore {
+    client: FirestoreClient,
+    event_store: FirestoreEventStore,
+}
 
 impl FirestoreChartStore {
     pub async fn new() -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
-        Ok(Self(FirestoreClient::new().await?))
+        Ok(Self {
+            client: FirestoreClient::new().await?,
+            event_store: FirestoreEventStore::new().await?,
+        })
     }
 
     async fn reader_get_impl(
@@ -29,7 +36,7 @@ impl FirestoreChartStore {
         Option<query_use_case::port::ChartQueryData>,
         Box<dyn std::error::Error + Send + Sync>,
     > {
-        self.0
+        self.client
             .get_document::<ChartDocumentData>(&path::chart_document(id))
             .await?
             .map(converter::query_data_from_document)
@@ -41,7 +48,7 @@ impl FirestoreChartStore {
     ) -> Result<Vec<query_use_case::port::ChartQueryData>, Box<dyn std::error::Error + Send + Sync>>
     {
         let documents = self
-            .0
+            .client
             .list_all_documents::<ChartDocumentData>(&path::chart_collection())
             .await?;
         let documents = documents
@@ -55,64 +62,31 @@ impl FirestoreChartStore {
         &self,
         id: ChartId,
     ) -> Result<Option<Chart>, Box<dyn std::error::Error + Send + Sync>> {
-        let event_stream = self
-            .0
-            .get_document::<EventStreamDocumentData>(&path::event_stream_document(&id.to_string()))
-            .await?;
-        if event_stream.is_none() {
-            return Ok(None);
-        }
-        let collection_path = path::event_collection();
-        let mut start_after = None;
-        let mut all_documents = vec![];
-        loop {
-            let documents = self
-                .0
-                .run_collection_query::<EventDocumentData>(
-                    &collection_path,
-                    Some(Filter::and([FieldPath::raw("stream_id")
-                        .equal(firestore_client::to_value(&id.to_string())?)?])),
-                    Some([FieldPath::raw("version").ascending()]),
-                    start_after.clone(),
-                    Some(100),
-                )
-                .await?;
-            let is_end = documents.is_empty() || documents.len() < 100;
-            start_after = Some([firestore_client::to_value(
-                &documents
-                    .last()
-                    .expect("documents to have at least one element")
-                    .fields
-                    .version,
-            )?]);
-            all_documents.extend(documents);
-            if is_end {
-                break;
-            }
-        }
-        let events = all_documents
+        let event_stream_id = EventStreamId::from_str(id.to_string().as_str())?;
+        let events = self
+            .event_store
+            .find_events_by_event_stream_id(&event_stream_id)
+            .await?
             .into_iter()
-            .map(converter::chart_event_from_document)
-            .collect::<Result<Vec<ChartEvent>, Box<dyn std::error::Error + Send + Sync>>>()?;
+            .map(|event| match event {
+                write_model::event::Event::Chart(event) => event,
+                write_model::event::Event::DataPoint(_) => unreachable!(),
+            })
+            .collect::<Vec<ChartEvent>>();
         Ok(Some(Chart::from_events(&events)?))
     }
 
     async fn repository_store_impl(
         &self,
         current: Option<Version>,
-        events: &[ChartEvent],
+        events: Vec<ChartEvent>,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        if events.is_empty() {
-            return Ok(());
-        }
-
-        let events = events.to_vec();
         let result = self
-            .run_transaction(move |transaction| {
-                Box::pin(async move {
-                    Self::repository_store_impl_transaction(transaction, current, events).await
-                })
-            })
+            .event_store
+            .store(
+                current,
+                events.into_iter().map(Event::from).collect::<Vec<Event>>(),
+            )
             .await;
 
         // To simplify the structure, update the query data at this timing (not supported for failure).
@@ -121,79 +95,18 @@ impl FirestoreChartStore {
         result
     }
 
-    async fn repository_store_impl_transaction(
-        transaction: &mut Transaction,
-        current: Option<Version>,
-        events: Vec<ChartEvent>,
-    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        let event_stream_id = EventStreamId::from_str(events[0].stream_id.to_string().as_str())?;
-        let last_event_version = events
-            .last()
-            .expect("events to have at least one element")
-            .version;
-        let last_event_at = events
-            .last()
-            .expect("events to have at least one element")
-            .at;
-        match current {
-            None => {
-                // create event_stream
-                transaction.create(
-                    &path::event_stream_document(event_stream_id.to_string().as_str()),
-                    &EventStreamDocumentData {
-                        id: event_stream_id.to_string(),
-                        last_event_at: last_event_at.to_string(),
-                        version: i64::from(last_event_version),
-                    },
-                )?;
-            }
-            Some(current) => {
-                // get event_stream with lock
-                let event_stream = transaction
-                    .get::<EventStreamDocumentData>(&path::event_stream_document(
-                        event_stream_id.to_string().as_str(),
-                    ))
-                    .await?
-                    .ok_or("event stream not found")?;
-
-                // check version
-                if event_stream.fields.version != i64::from(current) {
-                    return Err("version mismatch".into());
-                }
-
-                // update event_stream
-                transaction.update(
-                    &path::event_stream_document(event_stream_id.to_string().as_str()),
-                    &EventStreamDocumentData {
-                        last_event_at: last_event_at.to_string(),
-                        version: i64::from(last_event_version),
-                        ..event_stream.fields
-                    },
-                )?;
-            }
-        }
-        // create events
-        for event in events {
-            transaction.create(
-                &path::event_document(event.id),
-                &converter::event_document_data_from_chart_event(&event),
-            )?;
-        }
-        Ok(())
-    }
-
     async fn repository_store_impl_update_query_data(
         &self,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let updater_metadata_document_path = path::query_updater_document();
         let last_processed_event_at = self
-            .0
+            .client
             .get_document::<UpdaterMetadataDocumentData>(&updater_metadata_document_path)
             .await?
             .map(|document| document.fields.last_processed_event_at)
             .unwrap_or_else(|| "1970-01-01T00:00:00.000Z".to_owned());
         let events = self
-            .0
+            .client
             .run_collection_query::<EventDocumentData>(
                 &path::event_collection(),
                 Some(Filter::and([FieldPath::raw("at").greater_than_or_equal(
@@ -211,7 +124,7 @@ impl FirestoreChartStore {
                 .collection("processed_events")?
                 .doc(event.name.document_id().as_ref())?;
             let processed_event = self
-                .0
+                .client
                 .get_document::<UpdaterMetadataProcessedEventDocumentData>(&document_path)
                 .await?;
             if processed_event.is_none() {
@@ -319,7 +232,7 @@ impl FirestoreChartStore {
             >,
         >,
     {
-        let mut transaction = self.0.begin_transaction().await?;
+        let mut transaction = self.client.begin_transaction().await?;
         let result = match callback(&mut transaction).await {
             Ok(()) => transaction.commit().await.map_err(Into::into),
             Err(e) => Err(e),
@@ -375,7 +288,7 @@ impl command_use_case::port::ChartRepository for FirestoreChartStore {
         current: Option<Version>,
         events: &[ChartEvent],
     ) -> Result<(), command_use_case::port::chart_repository::Error> {
-        self.repository_store_impl(current, events)
+        self.repository_store_impl(current, events.to_vec())
             .await
             .map_err(command_use_case::port::chart_repository::Error::from)
     }
