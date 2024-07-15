e2ffab1d04b25fff2bae48eb0ce67dbeb6353b12
bouzuya <m@bouzuya.net>
2024-07-15 10:16:42 +0900

    Fix to use FirestoreDataPointStore

--- crates/main/src/subcommand/server.rs
+++ crates/main/src/subcommand/server.rs
@@ -1,7 +1,6 @@
-use std::{path::PathBuf, sync::Arc};
+use std::sync::Arc;
 
-use file_system_store::FileSystemDataPointStore;
-use firestore_store::FirestoreChartStore;
+use firestore_store::{FirestoreChartStore, FirestoreDataPointStore};
 
 pub async fn run() -> anyhow::Result<()> {
     let chart_store = Arc::new(
@@ -9,7 +8,11 @@ pub async fn run() -> anyhow::Result<()> {
             .await
             .map_err(|e| anyhow::anyhow!(e))?,
     );
-    let data_point_store = Arc::new(FileSystemDataPointStore::new(PathBuf::from("data")));
+    let data_point_store = Arc::new(
+        FirestoreDataPointStore::new()
+            .await
+            .map_err(|e| anyhow::anyhow!(e))?,
+    );
     let app = server::App::new(
         chart_store.clone(),
         chart_store,
