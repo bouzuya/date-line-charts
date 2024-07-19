cd026676863041dbea241d12547d4d0ab2fa165f
bouzuya <m@bouzuya.net>
2024-07-19 23:08:16 +0900

    Extract outputs and variables

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -6,16 +6,7 @@ module "registry" {
   source = "../../modules/artifact_registry"
 }
 
-variable "image_tag" {
-  type     = string
-  nullable = false
-}
-
 module "app" {
   source    = "../../modules/cloud_run"
   image_tag = var.image_tag
-}
-
-output "app_uri" {
-  value = module.app.uri
 }
---
+++ terraform/environments/dev/outputs.tf
@@ -0,0 +1,3 @@
+output "app_uri" {
+  value = module.app.uri
+}
